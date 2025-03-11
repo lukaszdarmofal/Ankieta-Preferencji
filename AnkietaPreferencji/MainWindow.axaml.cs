8f2182b53b5c36761d9f136c4324ed777ff66fe1
Łukasz Darmofał <lukasz.darmofal@cyfroweszkoly.pl>
2025-03-11 15:49:50 +0100

    Created basic app functionality and finished the project.

--- AnkietaPreferencji/MainWindow.axaml.cs
+++ AnkietaPreferencji/MainWindow.axaml.cs
@@ -10,8 +10,36 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
+        Button_Submit.Click += Submit_Clicked;
     }
     
+    private void Submit_Clicked(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            int counter = 0;
+            if (CheckBox_Question1.IsChecked == true)
+            {
+                counter++;
+            }
+            if (CheckBox_Question2.IsChecked == true)
+            {
+                counter++;
+            }
+            if (CheckBox_Question3.IsChecked == true)
+            {
+                counter++;
+            }
+            
+            var zainteresowanie = (ComboBox_Intrest.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "Nie wybrano";
+                
+            TextBlock_Summary.Text = $"Imię: {TextBox_InputName.Text}\nZainteresowanie: {zainteresowanie}\nOdpowiedziałeś TAK {counter} razy.";
+
+        }
+        catch (Exception ex)
+        {
+            TextBlock_Summary.Text = ex.Message;
+        }
+    }
     
 }
